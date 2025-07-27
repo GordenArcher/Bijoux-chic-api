ba55bb59e43668efe567192401ea94ad19ea0990
GordenArcher <archergorden@gmail.com>
2025-07-27 07:28:40 +0000

    csrf fix-3

--- core/settings.py
+++ core/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         'users.authentication.CookieTokenAuthentication'
     )
 }
@@ -73,11 +73,11 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 
-CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS
+# CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS
 
-CSRF_COOKIE_HTTPONLY = False
-CSRF_COOKIE_SAMESITE = "None"
-CSRF_COOKIE_SECURE = True  
+# CSRF_COOKIE_HTTPONLY = False
+# CSRF_COOKIE_SAMESITE = "None"
+# CSRF_COOKIE_SECURE = True  
 
 SESSION_COOKIE_SAMESITE = "None"
 SESSION_COOKIE_SECURE = True
