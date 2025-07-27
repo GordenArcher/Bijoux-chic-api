97110d09d5154ba71d619dbc99a8f1de3588bc50
GordenArcher <archergorden@gmail.com>
2025-07-27 07:33:02 +0000

    settings fix

--- core/settings.py
+++ core/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         # 'rest_framework.authentication.SessionAuthentication',
-        'users.authentication.CookieTokenAuthentication'
+        'users.authentication.CookieTokenAuthentication',
     )
 }
 
